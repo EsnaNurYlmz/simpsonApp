127aaa72f5bb917f5d4b08c8f8557cea425ad52b
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2023-08-09 17:56:55 +0300

    Initial Commit

---
+++ simpson App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpson App
+//
+//  Created by Esna nur Yılmaz on 9.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
